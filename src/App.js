3f7314266ec3e87793b17d09e1a8a86b42f928b7
KRISNAN ARMA <krisnanddd@gmail.com>
2022-08-20 21:41:33 +0700

    part6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import WordCard from './WordCard'
 import './App.css';
+
 const word = "Hello";
 function App() {
- return (
- <div>
- {
-    <WordCard value={word}/>
- }
+    return (
+   <div>
+   
+       <WordCard value={word}/>
+ 
  </div>
  );
 }
